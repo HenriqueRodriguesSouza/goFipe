e9aee36f72093b2ca303f752352d0d9cb6f05e59
Lucas Braich <89614811+braiklu@users.noreply.github.com>
2024-05-20 19:35:55 -0300

    BackEnd Formulário

--- core/models.py
+++ core/models.py
@@ -148,3 +148,75 @@ class ChamadaApi:
             return response.json()
         else:
             return None
+
+
+class RetornoFormulario:
+    @staticmethod
+    def get_tabela():
+        tabelas = ChamadaApi.consultar_tabela()
+        form_format = [
+            (tabela['Codigo'], tabela['Mes']) for tabela in tabelas
+        ]
+        return form_format
+
+    @staticmethod
+    def get_lista_marcas(tabela: int):
+        lista_marcas = ChamadaApi.consultar_marcas(tabela, 1)
+        form_format = [
+            (item['Value'], item['Label']) for item in lista_marcas
+        ]
+        return form_format
+
+    @staticmethod
+    def get_veiculos(tabela: int, marca: int):
+        lista_carros = []
+        tipo_veiculo = 1
+        modelos_marca = ChamadaApi.consultar_veiculos_marca(tabela, tipo_veiculo, marca)
+
+        for carro in modelos_marca['Modelos']:
+            lista_carros.append(carro)
+
+        form_format = [
+            (item['Value'], item['Label']) for item in lista_carros
+        ]
+        return form_format
+
+    @staticmethod
+    def get_anos_veiculo(tabela: int, marca: int, veiculo: int):
+        tipo_veiculo = 1
+        veiculos_anos = ChamadaApi.consultar_anos_veiculo(tabela, tipo_veiculo, marca, veiculo)
+
+        lista_anos = []
+        for modelo in veiculos_anos:
+            ano = modelo['Value'].split("-")[0]
+            combustivel = modelo['Value'].split("-")[1]
+            modelo.update({'Ano': ano})
+            modelo.update({'TipoCombustivel': combustivel})
+            lista_anos.append(modelo)
+
+        form_format = [
+            (item['Value'], item['Label']) for item in veiculos_anos
+        ]
+        return form_format, veiculos_anos
+
+    @staticmethod
+    def get_versoes_veiculo(tabela: int, marca: int, ano: str, combustivel: int, ano_modelo: int):
+        tipo_veiculo = 1
+        modelos = ChamadaApi.consultar_modelo(tabela, tipo_veiculo, marca, ano, combustivel, ano_modelo)
+
+        form_format = [
+            (item['Value'], item['Label']) for item in modelos
+        ]
+
+        return form_format
+
+    @staticmethod
+    def consulta_valor(tabela: int, marca: int, ano: str, tipo_combustivel: int, ano_modelo: int, codigo_modelo: int):
+        tipo_consulta = "tradicional"
+        tipo_veiculo = 1
+        consulta = ChamadaApi.consulta_valor(tabela, tipo_veiculo, marca, ano, tipo_combustivel, ano_modelo,
+                                             codigo_modelo, tipo_consulta)
+        valor_formatado = int(consulta['Valor'].replace('.', '').replace('R$', '').replace(',00', ''))
+        form_format = (consulta['CodigoFipe'], valor_formatado, consulta['MesReferencia'])
+
+        return form_format
